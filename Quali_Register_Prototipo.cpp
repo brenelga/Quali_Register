c6f69d35e8a126766ecddd3cd04d63b12f2a9909
brenelga <123479074+brenelga@users.noreply.github.com>
2023-03-29 23:22:44 -0600

    Update Quali_Register_Prototipo.cpp

--- Quali_Register_Prototipo.cpp
+++ Quali_Register_Prototipo.cpp
@@ -9,7 +9,7 @@ TSU en Desarrollo y Gestion de Software
 #include<iostream>
 using namespace std;
 int main() {
-	int calif, grado, inicio; //Variables que nos guardaran las calificaciones, el grado al que le dará clase y las opciones del menu
+	int calif, grado, inicio, menu; //Variables que nos guardaran las calificaciones, el grado al que le dará clase y las opciones del menu
 	short matricula; //Aqui se guardará la matrícula del alumno
 	string ApAl, AmAl, nAl, ApM, AmM, Nm, Evid, Asig, user, password;//Apellido Materno,Paterno,Nombre del Alumno; Apellido Paterno,Materno y Nombre del Maestro; Evidencia, Asignatura, usuario y contraseña
 	char grupo; //Grupo
@@ -41,7 +41,17 @@ int main() {
 		if (user == user && password == password) {
 			cout << "Datos correctos \n";
 			do {
-				cout<< "Bienvenido" << user <<
+				cout<< "Bienvenido" << user << "\n";
+				cout<<"Menu Principal \n";
+				cout<<"1: Registro de Alumno \n";
+				cout<<"2: Registro de Evidencia \n";
+				cout<<"3: Asignar Calificacion \n";
+				cin>>menu;
+				switch(menu){
+					case 1:
+						cout<<"Ingrese el primer apellido \n";
+						cin<<
+				}
 			} while (inicio == 0);
 		}
 		break;
