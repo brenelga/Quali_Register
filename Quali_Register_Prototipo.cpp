44abba30813f9536ee41b5cc068b95fa1b340f98
brenelga <123479074+brenelga@users.noreply.github.com>
2023-03-30 13:24:40 -0600

    Update Quali_Register_Prototipo.cpp

--- Quali_Register_Prototipo.cpp
+++ Quali_Register_Prototipo.cpp
@@ -13,47 +13,10 @@ int main() {
 	short matricula; //Aqui se guardará la matrícula del alumno
 	string ApAl, AmAl, nAl, ApM, AmM, Nm, Evid, Asig, user, password;//Apellido Materno,Paterno,Nombre del Alumno; Apellido Paterno,Materno y Nombre del Maestro; Evidencia, Asignatura, usuario y contraseña
 	char grupo; //Grupo
-	cout << "----Bienvenido a Quali-Register----\n";//Mensaje de Bienvenida
-	cout << "-----V.0.0.1----- \n"; //Version
-	cout << "Seleccione una opcion \n"; //Inicio de menu
-	cout << "1: Primer Uso \n"; //Opcion 1 Menu
-	cout << "2: Ya estoy registrado \n"; //Opcion 2 Menu
-	cin >> inicio; //Ingresa las opciones
-	switch (inicio) { //Dependiendo de la opcion que ingrese el usuario, se hará lo siguiente
-	case 1: //Registra el nuevo usuario, esto se hará cuando se use por primera vez
-		cout << "Ingrese su Primer Apellido \n";
-		cin >> ApM;
-		cout << "Ingrese su Segundo Apellido \n";
-		cin >> AmM;
-		cout << "Ingrese su nombre \n";
-		cin >> Nm;
-		cout << "Ingrese su usuario \n";
-		cin >> user;
-		cout << "Ingrese su password \n";
-		cin >> password;
-		cout << "Registro guardado de manera exitosa \n";
-		break;
-	case 2: //Si ya está registradio, iniciará sesión
-		cout << "Ingrese su usuario \n";
-		cin >> user;
-		cout << "Ingrese su contraseña \n";
-		cin >> password;
-		if (user == user && password == password) {
-			cout << "Datos correctos \n";
-			do {
-				cout<< "Bienvenido" << user << "\n";
-				cout<<"Menu Principal \n";
-				cout<<"1: Registro de Alumno \n";
-				cout<<"2: Registro de Evidencia \n";
-				cout<<"3: Asignar Calificacion \n";
-				cin>>menu;
-				switch(menu){
-					case 1:
-						cout<<"Ingrese el primer apellido \n";
-						cin<<
-				}
-			} while (inicio == 0);
-		}
-		break;
-	}
-}
+	do{
+		cout<<"----------------------------- \n";
+		cout<<"-Bienvenido a Quali-Register- \n";
+		cout<<"----------------------------- \n";
+		cout<<"\n";
+		cout<<"Menu Principal";
+		cout<<"1: Registro de Usuarios \n";
