200059e4d9ee574a2a7667c698818c398c708f9a
brenelga <123479074+brenelga@users.noreply.github.com>
2023-03-31 11:01:25 -0600

    Update Quali_Register_Prototipo.cpp

--- Quali_Register_Prototipo.cpp
+++ Quali_Register_Prototipo.cpp
@@ -9,7 +9,7 @@ TSU en Desarrollo y Gestion de Software
 #include<iostream>
 using namespace std;
 int main() {
-	int calif, grado, inicio, menu; //Variables que nos guardaran las calificaciones, el grado al que le dará clase y las opciones del menu
+	int calif, grado, inicio, menu, id; //Variables que nos guardaran las calificaciones, el grado al que le dará clase y las opciones del menu
 	short matricula; //Aqui se guardará la matrícula del alumno
 	string ApAl, AmAl, nAl, ApM, AmM, Nm, Evid, Asig, user, password, resp;//Apellido Materno,Paterno,Nombre del Alumno; Apellido Paterno,Materno y Nombre del Maestro; Evidencia, Asignatura, usuario y contraseña
 	char grupo; //Grupo
@@ -68,6 +68,8 @@ int main() {
 								cin>>AmAl;
 								cout<<"Ingrese el nombre \n";
 								cin>>nAl;
+								cout<<"Ingrese la matricula \n";
+								cin>>matricula;
 								cout<<"Ingrese el grado \n";
 								cin>>grado;
 								cout<<"Ingrese el grupo \n";
@@ -88,10 +90,86 @@ int main() {
 								cin>>Evid;
 								cout<<"Ingresa la asignatura \n";
 								cin>>Asig;
-								
-							}
+								cout<<"Ingrese el id de la tarea \n";
+								cin>>id;
+								cout<<"Registro Exitoso \n";
+								cout<<"Desea Realizar otro registro \n";
+								cout<<"si \n";
+								cout<<"no \n";
+								cin>>resp;
+							} while (resp=="si" || resp=="Si" || resp=="SI" || resp=="sI");
+						break;
+						case 3:
+							do{
+								cout<<"--Registro de Calificaciones-- \n";
+								cout<<"--------Quali-Register-------- \n";
+								cout<<"\n";
+								cout<<"Ingrese la matricula del alumno \n";
+								cin<<matricula;
+								cout<<"Ingrese el id de la tarea \n";
+								cin>>id;
+								cout<<"Ingrese la calificacion del estudiante \n";
+								cin>>calif;
+								cout<<"Calificacion guardada \n";
+								cout<<"Desea continuar \n";
+								cout<<"si \n";
+								cout<<"no  \n";
+								cin>>resp;
+							} while (resp=="si" || resp=="Si" || resp=="SI" || resp=="sI");
+						break;
+						case 4:
+							cout<<"-----------Manual de Usuario----------------- \n";
+							cout<<"-----------Menu de Registros----------------- \n";
+							cout<<"-------------Quali-Register------------------ \n";
+							do{
+								cout<<"Seleccione el menu del que desea ayuda \";
+								cout<<"1: Registrar Alumno \n";
+								cout<<"2: Registrar Evidencia \n";
+								cout<<"3: Registrar Calificacion \n";
+								cin>>inicio;
+								switch (inicio){
+									case 1:
+										cout<<"----Menu Registrar Alumno---- \n";
+										cout<<"\n";
+										cout<<"Al acceder a este menu, nos pedira los siguientes datos:  \n";
+										cout<<"1: Primer Apellido del Alumno \n";
+										cout<<"2: Segundo Apellido del Alumno \n";
+										cout<<"3: Nombre del Alumno(Un solo nombre) \n";
+										cout<<"4: Matricula, este sera el dato identificador del alumno en la BDD \n";
+										cout<<"5: Grado, el cuatrimestre que esta cursando (solo numeros) \n";
+										cout<<"6: Grupo, el grupo en el que se encuentra (una sola letra) \n";
+									break;
+									case 2:
+										cout<<"----Menu Registrar Evidencias---- \n";
+										cout<<"\n";
+										cout<<"Al acceder nos pedira los siguientes datos: \n";
+										cout<<"1: El titulo de la actividad \n";
+										cout<<"2: La asignatura a la que pertenece \n";
+										cout<<"3: id de la actividad, servira para identificar la actividad en la BDD (solo numeros) \n";
+									break;
+									case 3:
+										cout<<"----Menu Registrar Calificacion---- \n";
+										cout<<"\n";
+										cout<<"Al acceder nos pedira ingresar los siguientes datos: \n";
+										cout<<"1: Matricula, nos permitira ubicar al alumno en la BDD \n";
+										cout<<"2: id de Evidencia, nos permitirá ubicar al alumno en la BDD \n";
+										cout<<"3: Calificacion, la calificacion del alumno \n";
+									break;
+									default cout<<"Opcion no reconocida, intentelo de nuevo \n";
+								}
+								cout<<"Quieres intentarlo de nuevo \n";
+								cout<<"si \n";
+								cout<<"no \n";
+								cin>>resp;
+							} while (resp=="Si" || resp=="si" || resp=="SI" || resp=="sI");
+							break;
+							default cout<<"Opcion no reconocida intentelo de nuevo \n";
 					}
-					}
+					cout<<"Quieres intentarlo de nuevo \n";
+					cout<<"Si \n";
+					cout<<"No \n";
+					cin>>resp;
+					}while (resp=="Si" || resp=="si" || resp=="sI" || resp=="SI");
 				}
 			break;
 		}
